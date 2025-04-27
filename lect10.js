e4082fa662444636be633fb20deb0e1060ce90ab
mehulbest <157376178+mehulbest@users.noreply.github.com>
2025-04-28 02:45:36 +0530

    Initial commit - Currency Converter project

---
+++ lect10.js
@@ -0,0 +1,67 @@
+const BASE_URL = "https://open.er-api.com/v6/latest";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+// Populate dropdowns
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = true;
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = true;
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+// Update flag images
+function updateFlag(element) {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/shiny/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+}
+
+// Fetch exchange rate and update message
+async function updateExchangeRate() {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  const URL = `${BASE_URL}/${fromCurr.value}`;
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    let rate = data.rates[toCurr.value];
+    let finalAmount = (amtVal * rate).toFixed(2);
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+  } catch (error) {
+    msg.innerText = "Something went wrong. Please try again later.";
+  }
+}
+
+// Button click to update rate
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+// **Fetch actual rate immediately when page loads**
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
